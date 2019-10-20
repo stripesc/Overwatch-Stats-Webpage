1874532c3b81e12b1b7b4f46c224b5bf5fd58be0
stripesc <44383867+stripesc@users.noreply.github.com>
2019-10-20 20:24:40 -0300

    Changed hero selector
    Changed hero selector from an text input to a selector

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ function searchHero(){
 }
 
 function setPlayer(){
+	jsonDiv.innerHTML = ""
 	var bTag = document.getElementById("bTag").value;
 	bTag = bTag.replace(/#/, "-")
 	console.log(bTag)
@@ -32,6 +33,48 @@ function setPlayer(){
 	xmlHttp.open("GET", url, false)
 	xmlHttp.send()
 	json = JSON.parse(xmlHttp.responseText)
+	if(json.error == "bad tag"){
+		jsonDiv.innerHTML = "No character exists under BattleTag \""  + bTag + "\""
+		return
+	}
+	if (json["private"]){
+		jsonDiv.innerHTML = "Profile is private"
+	}
+	else{
+		heroInput.innerHTML = "<select id='hero' type='text' name='hero'>" +
+				"<option value='ana'>Ana</option>" +
+				"<option value='ashe'>Ashe</option>" +
+				"<option value='baptiste'>Baptiste</option>" +
+				"<option value='bastion'>Bastion</option>" +
+				"<option value='brigitte'>Brigitte</option>" +
+				"<option value='doomfist'>Doomfist</option>" +
+				"<option value='dVa'>D.Va</option>" +
+				"<option value='genji'>Genji</option>" +
+				"<option value='hanzo'>Ganzo</option>" +
+				"<option value='junkrat'>Junkrat</option>" +
+				"<option value='lucio'>Lucio</option>" +
+				"<option value='mccree'>Mccree</option>" +
+				"<option value='mei'>Mei</option>" +
+				"<option value='mercy'>Mercy</option>" +
+				"<option value='moira'>Moira</option>" +
+				"<option value='orisa'>Orisa</option>" +
+				"<option value='pharah'>Pharah</option>" +
+				"<option value='reaper'>Reaper</option>" +
+				"<option value='reinhardt'>Reinhardt</option>" +
+				"<option value='roadhog'>Roadhog</option>" +
+				"<option value='sigma'>Sigma</option>" +
+				"<option value='soldier76'>Soldier76</option>" +
+				"<option value='sombra'>Sombra</option>" +
+				"<option value='symmetra'>Symmetra</option>" +
+				"<option value='torbjorn'>Torbjorn</option>" +
+				"<option value='tracer'>Tracer</option>" +
+				"<option value='widowmaker'>Widowmaker</option>" +
+				"<option value='winston'>Winston</option>" +
+				"<option value='wreckingBall'>WreckingBall</option>" +
+				"<option value='zarya'>Zarya</option>" +
+				"<option value='zenyatta'>Zenyatta</option>" +
+			"</select> <button id='heroSubmit' type='button' onClick='searchHero()'>Search Hero Stats</button>"
+	}
 }
 
 function getHeroData(hero){
